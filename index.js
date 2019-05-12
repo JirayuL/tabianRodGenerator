f74c1a1ceec5a0e5344d4447aff87a176c4bee9d
Jirayu Laungwilawan <jirayu.l@ku.th>
2019-05-12 16:01:15 +0700

    Resolve bug

--- index.js
+++ index.js
@@ -1,39 +1,55 @@
 function main() {
-  console.log('Start');
+  console.log("Start");
 
-  let notWant = [1, 3, 6, 0]
-  let notWantSum = [13]
-  let wantEndWith = 9
+  let notWant = ["1", "3", "6", "7", "0"];
+  let notWantSum = [13];
 
-  let startWith = 8
+  let wantSumEndWith = 9;
 
-  for (let i = 0; i < 9999; i++) {
-    let splitNum = i.toString().split('')
-    let sum = startWith + splitNum.reduce((partial_sum, n) => partial_sum + parseInt(n), 0)
+  let startWith = 8;
+
+  for (let i = 0; i < 10000; i++) {
+    let splitNum = i.toString().split("");
+    let sum =
+      startWith +
+      splitNum.reduce((partial_sum, n) => partial_sum + parseInt(n), 0);
     // console.log(sum);
     // console.log(sum + startWith);
 
-    let sumStr = sum.toString()
+    let sumStr = sum.toString();
     // console.log(sumStr);
 
-    let result = '-1'
+    let result = "-1";
+    let legal = true;
     for (let j = 0; j < notWant.length; j++) {
-      if (checkElementNotInArray(splitNum, notWant[j]) && checkElementNotInArray(notWantSum, sum) && sumStr.endsWith(wantEndWith)) {
-        result = startWith + ' ' + i + ' sum = ' + sumStr
+      if (
+        checkElementNotInArray(splitNum, notWant[j]) &&
+        checkElementNotInArray(notWantSum, sum) &&
+        sumStr.endsWith(wantSumEndWith)
+      ) {
+        // console.log(splitNum);
+        // console.log(notWant[j]);
+
+        // console.log(true);
+
+        result = startWith + " " + i + " sum = " + sumStr;
         // console.log(startWith + ' ' + i)
+      } else {
+        // console.log(false);
+
+        legal = false;
       }
     }
-    if (result != '-1') {
+    if (result != "-1" && legal) {
       console.log(result);
     }
   }
 
-  console.log('End');
-
+  console.log("End");
 }
 
 function checkElementNotInArray(arr, element) {
-  return arr.indexOf(element) < 0
+  return arr.indexOf(element) < 0;
 }
 
-main()
+main();
